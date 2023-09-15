374ce1531b684ad65397ee57e9f52bef0da0b467
gabriel-fcorrea <gabrecorrea@gmail.com>
2023-09-14 21:35:38 -0300

    Version-day 14.09 1.2

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -3,12 +3,27 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+    //declarar uma variável global do tipo de objeto do layout
+    ImageView imgLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+        imgLogo = findViewById(R.id.imgLogo);
+
+    // criando um evento de click na imagem
+
+        imgLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
