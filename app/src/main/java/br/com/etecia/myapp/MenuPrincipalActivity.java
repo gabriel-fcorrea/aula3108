16d6a9a1758b9d48670bad1796f96d8628ba8a8d
gabriel-fcorrea <gabrecorrea@gmail.com>
2023-09-14 22:08:35 -0300

    Version-day 14.09 1.5

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
 
     MaterialCardView cardAeroModelos;
+    MaterialCardView cardGaleria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +20,18 @@ public class MenuPrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.menu_principal_layout);
 
         cardAeroModelos = findViewById(R.id.cardAeroModelos);
+        cardGaleria = findViewById(R.id.cardGaleria);
 
         cardAeroModelos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MenuPrincipalActivity.this, LoginActivity.class));
+            }
+        });
+        cardGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Cliquei em Galeria",Toast.LENGTH_SHORT).show();
             }
         });
     }
