69486cffe9887cafa4f6d9e99ab663cbf2016a23
gabriel-fcorrea <gabrecorrea@gmail.com>
2023-09-14 22:00:50 -0300

    Version-day 14.09 1.4

--- app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MenuPrincipalActivity.java
@@ -2,13 +2,28 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MenuPrincipalActivity extends AppCompatActivity {
+
+    MaterialCardView cardAeroModelos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal_layout);
+
+        cardAeroModelos = findViewById(R.id.cardAeroModelos);
+
+        cardAeroModelos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MenuPrincipalActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
